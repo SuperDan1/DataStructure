9cd8c68ae23d8c493d1b48aa66ebd0bad7794b05
superdan <futingyang123@163.com>
2021-05-15 19:36:06 +0800

    complete List test

---
+++ tests/SeqListTest.cc
@@ -0,0 +1,21 @@
+#include "List.h"
+#include "gtest/gtest.h"
+
+TEST(List, test001)
+{
+    int arry[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // 三种创建类对象的方法
+    SeqList<int> seqlist;
+    SeqList<int> seqlist1(arry, 10);
+    SeqList<int> *seqlist2 = new SeqList<int>(arry, 10);
+    cout << seqlist1.get(5) << endl;
+    cout << seqlist2->get(5) << endl;
+    cout << seqlist1.locate(10) << endl;
+    cout << seqlist2->locate(10) << endl;
+    seqlist1.insert(3, 11);
+    seqlist2->insert(4, 12);
+    seqlist1.Delete(1);
+    seqlist2->Delete(1);
+    seqlist1.printList();
+    seqlist2->printList();
+}
